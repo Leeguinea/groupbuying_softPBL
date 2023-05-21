3d6edddbe3e9f701b66a854e5a5fa25a06d0142e
Leehyeonjoo <lee101509@naver.com>
2023-05-22 05:06:19 +0900

    hyeonjoo

--- src/main/java/com/example/GroupBuying/controller/BoardController.java
+++ src/main/java/com/example/GroupBuying/controller/BoardController.java
@@ -1,7 +1,10 @@
 package com.example.GroupBuying.controller;
 
 import com.example.GroupBuying.dto.BoardDTO;
+import com.example.GroupBuying.dto.BoardFileDTO;
 import com.example.GroupBuying.entity.Board;
+import com.example.GroupBuying.entity.BoardFile;
+import com.example.GroupBuying.repository.BoardFileRepository;
 import com.example.GroupBuying.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -17,6 +21,12 @@ import java.util.List;
 public class BoardController {
 
     private final BoardService boardService;
+    private final BoardFileRepository boardFileRepository;
+
+    @GetMapping("/home")
+    public String homeForm(){
+        return "home";
+    }
 
     @GetMapping("/gesizak")
     public String writeForm(Model model, HttpSession session) {
@@ -28,19 +38,11 @@ public class BoardController {
         return "gesi_zak";
     }
 
-    @PostMapping("/gesizak")
-    public String write(BoardDTO boardDTO, Model model) {
-        boardService.write(boardDTO);
-        List<Board> boardList =boardService.findAll();
-        model.addAttribute("boardList", boardList);
-        return "gesi";
-    }
-
     @GetMapping("/gesi")
     public String postForm(Model model, String searchKey){
         List<Board> boardList = null;
         if(searchKey==null) {
-            boardList = boardService.findAll();
+            boardList = boardService.findAllByOrderByidDesc();
         } else {
             boardList = boardService.searchKeyList(searchKey);
         }
@@ -61,6 +63,26 @@ public class BoardController {
         myboardList = boardService.findByWriter(loginId); // 로그인 아이디로 작성자가 일치하는 게시물을 가져옴
         model.addAttribute("boardList",myboardList);
         return "love";
+    }
+
+    @PostMapping("/gesizak")
+    public String write(BoardDTO boardDTO, BoardFileDTO boardFileDTO, Model model) throws IOException {
+        boardService.write(boardDTO, boardFileDTO);
+        List<Board> boardList =boardService.findAllByOrderByidDesc();
+        model.addAttribute("boardList", boardList);
+        return "gesi";
+    }
+
+    @GetMapping("/view")
+    public String detailForm(Model model, Long id) {
+
+        BoardFile boardFile = boardFileRepository.findByBoard_id(id);
+        Board board =  boardService.findById(id);
+        String storedFileName = boardFile.getStoredFileName();
+        String imageSrc = "/upload/" + storedFileName;
+        model.addAttribute("board", board);
+        model.addAttribute("imageSrc", imageSrc);
+        return "snagse_page";
     }
 
 }
